b49aaf48bc27094501db89e87ef3f3260a024b07
SonicRay241 <rayhan.permana@yahoo.com>
2023-01-19 08:07:13 +0800

    Initial Commit

---
+++ GardenAppNative/GardenAppNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenAppNativeApp.swift
+//  GardenAppNative
+//
+//  Created by Rayhan Permana on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GardenAppNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
